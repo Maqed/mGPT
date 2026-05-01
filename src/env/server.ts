6f4663c0e8266e73c920298752fde58d193ae0e4
Maged Ibrahim <magedibra@yahoo.com>
2026-05-01 17:50:37 +0300

    feat: add env

---
+++ src/env/server.ts
@@ -0,0 +1,14 @@
+import { createEnv } from "@t3-oss/env-core";
+import * as z from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.url(),
+    NEXT_PUBLIC_BASE_URL: z.url().default("http://localhost:3000"),
+    
+    GROQ_API_KEY: z.string(),
+
+    REDIS_URL: z.string(),
+  },
+  runtimeEnv: process.env,
+});
